e2ae2e15ea41f31210c71702f4c0ffdbae8c6f32
Fealrone <alzthejerk@yahoo.com>
2012-11-29 22:04:31 +0800

    Finished Settings' edit username and edit password.

--- src/com/alslimsibqueryu/anow/Settings.java
+++ src/com/alslimsibqueryu/anow/Settings.java
@@ -1,10 +1,25 @@
 package com.alslimsibqueryu.anow;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,10 +31,43 @@ import android.widget.Toast;
 public class Settings extends Activity implements OnClickListener {
 
 	Button btnChangeUname, btnChangePword, btnChangeProfPic, btnLogout;
+	View textEntryViewU, textEntryViewP;
+	int updateSuccess;
+	String username, new_username, new_password, password, picturePath;
+	ApplicationController AP;
+	
 	// Header views
 	TextView tvTitle;
 	Button btnBack;
-
+	private ProgressDialog pDialog;
+	JSONParser jsonParser = new JSONParser();
+	
+	// url to create a new account for user
+	private static String url_edit_username_password = "http://10.0.2.2/ANowPhp/edit_username_password.php";
+	
+	//JSON Node names
+	private static final String TAG_SUCCESS = "success";
+	private static final int RESULT_LOAD_IMAGE = 1;
+		
+	private String hashPassword(String password){
+		String hashed = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(password.getBytes());
+			byte byteData[] = md.digest();
+			
+			StringBuffer sb = new StringBuffer();
+			for(int i=0; i < byteData.length; i++)
+				 sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			hashed = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return hashed.substring(0, 39);
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -27,6 +75,29 @@ public class Settings extends Activity implements OnClickListener {
 		setContentView(R.layout.settings);
 		this.setup();
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+         
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+ 
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+ 
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            
+            Toast.makeText(Settings.this, picturePath, Toast.LENGTH_SHORT).show();
+         
+        }
+	}
+
 
 	private void setup() {
 		// Set-up views
@@ -57,20 +128,29 @@ public class Settings extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.btnChangeUname:
+			LayoutInflater factoryU = LayoutInflater.from(Settings.this);
+			textEntryViewU = factoryU.inflate(R.layout.change_username, null);
 			AlertDialog.Builder alertU = new AlertDialog.Builder(Settings.this);
 			alertU.setTitle("Change email address:");
-			EditText newUsername = new EditText(Settings.this);
-			newUsername.setText(btnChangeUname.getText());
-			alertU.setView(newUsername);
-
+			alertU.setView(textEntryViewU);
+			
 			alertU.setPositiveButton("Save",
 					new DialogInterface.OnClickListener() {
 
 						public void onClick(DialogInterface dialog, int which) {
 							// TODO Auto-generated method stub
-							Toast.makeText(Settings.this,
-									"Username successfully changed!",
-									Toast.LENGTH_SHORT).show();
+							updateSuccess = 0;
+							AP = (ApplicationController)getApplicationContext();
+							username = AP.getUsername();
+							final EditText etNewU = (EditText)textEntryViewU.findViewById(R.id.etChangeUsername);
+							new_username = etNewU.getText().toString();
+							final EditText etP = (EditText)textEntryViewU.findViewById(R.id.etConfirmPasswordU);
+							password = hashPassword(etP.getText().toString());
+							
+							if((new_username.length() == 0 || etP.getText().toString().length() == 0))
+								Toast.makeText(Settings.this, "Required field(s) is missing!", Toast.LENGTH_SHORT).show();
+							else
+								new EditUsername().execute();
 						}
 					});
 			alertU.setNegativeButton("Cancel",
@@ -83,25 +163,29 @@ public class Settings extends Activity implements OnClickListener {
 			alertU.show();
 			break;
 		case R.id.btnChangePassword:
-			LayoutInflater factory = LayoutInflater.from(Settings.this);
-			View textEntryView = factory
-					.inflate(R.layout.change_password, null);
+			LayoutInflater factoryP = LayoutInflater.from(Settings.this);
+			textEntryViewP = factoryP.inflate(R.layout.change_password, null);
 			AlertDialog.Builder alertP = new AlertDialog.Builder(Settings.this);
 			alertP.setTitle("Change password:");
-			alertP.setView(textEntryView);
-
-			// EditText newPassword =
-			// (EditText)findViewById(R.id.etChangePassword);
-			// EditText newConfirmPassword =
-			// (EditText)findViewById(R.id.etChangeConfirmPassword);
+			alertP.setView(textEntryViewP);
+			
 			alertP.setPositiveButton("Save",
 					new DialogInterface.OnClickListener() {
 
 						public void onClick(DialogInterface dialog, int which) {
 							// TODO Auto-generated method stub
-							Toast.makeText(Settings.this,
-									"Password successfully changed!",
-									Toast.LENGTH_SHORT).show();
+							updateSuccess = 0;
+							AP = (ApplicationController)getApplicationContext();
+							username = AP.getUsername();
+							final EditText etNewP = (EditText)textEntryViewP.findViewById(R.id.etChangePassword);
+							new_password = hashPassword(etNewP.getText().toString());
+							final EditText etP = (EditText)textEntryViewP.findViewById(R.id.etConfirmPasswordP);
+							password = hashPassword(etP.getText().toString());
+							
+							if((new_password.length() == 0 || etP.getText().toString().length() == 0))
+								Toast.makeText(Settings.this, "Required field(s) is missing!", Toast.LENGTH_SHORT).show();
+							else
+								new EditPassword().execute();
 						}
 					});
 			alertP.setNegativeButton("Cancel",
@@ -114,9 +198,12 @@ public class Settings extends Activity implements OnClickListener {
 			alertP.show();
 			break;
 		case R.id.btnChangeProfPic:
-			Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-			photoPickerIntent.setType("image/*");
-			startActivityForResult(photoPickerIntent, 1);
+			Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, 
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			
+			//photoPickerIntent.setType("image/*");
+			//startActivityForResult(photoPickerIntent, 1);
+			startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
 			break;
 		case R.id.btnLogout:
 			AlertDialog.Builder alertOut = new AlertDialog.Builder(
@@ -141,5 +228,112 @@ public class Settings extends Activity implements OnClickListener {
 			alertOut.show();
 			break;
 		}
+		
 	}
+	
+	class EditUsername extends AsyncTask<String, String, String>{
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			pDialog = new ProgressDialog(Settings.this);
+			pDialog.setMessage("Updating Profile...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+		
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("new_username", new_username));
+			params.add(new BasicNameValuePair("password", password));
+			params.add(new BasicNameValuePair("username", username));
+			
+			// Send modified data through HTTP request
+			JSONObject json = jsonParser.makeHttpRequest(url_edit_username_password, params);
+						
+			// Check json success tag
+			try
+			{
+				if(json.getInt(TAG_SUCCESS) == 1)
+					updateSuccess = 1;
+				
+			} 
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+	
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			pDialog.dismiss();
+			
+			if(updateSuccess == 1)
+			{
+				Toast.makeText(Settings.this, "Username successfully edited!", Toast.LENGTH_SHORT).show();
+				AP.setUsername(new_username);
+			}
+			else
+				Toast.makeText(Settings.this, "Confirm password is incorrect!", Toast.LENGTH_SHORT).show();
+		}
+		
+	}
+	
+	class EditPassword extends AsyncTask<String, String, String>{
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			pDialog = new ProgressDialog(Settings.this);
+			pDialog.setMessage("Updating Profile...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+		
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("new_password", new_password));
+			params.add(new BasicNameValuePair("password", password));
+			params.add(new BasicNameValuePair("username", username));
+			
+			// Send modified data through HTTP request
+			JSONObject json = jsonParser.makeHttpRequest(url_edit_username_password, params);
+						
+			// Check json success tag
+			try
+			{
+				int success = json.getInt(TAG_SUCCESS);
+				
+				if(success == 1)
+					updateSuccess = 1;
+				
+			} 
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+	
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			pDialog.dismiss();
+			if(updateSuccess == 1)
+				Toast.makeText(Settings.this, "Password successfully edited!", Toast.LENGTH_SHORT).show();
+			else
+				Toast.makeText(Settings.this, "Confirm password is incorrect!", Toast.LENGTH_SHORT).show();
+		}
+		
+	}	
 }
